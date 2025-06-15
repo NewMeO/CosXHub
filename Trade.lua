f26debcbf4160b6ad335ad2c03f6e2538745244a
NewMeO <90089711+NewMeO@users.noreply.github.com>
2025-06-16 01:21:17 +0700

    Create Trade.lua

---
+++ Trade.lua
@@ -0,0 +1,11 @@
+_G.Settings = {
+    JobId = "d125100d-173c-4b83-b8ef-5810f35b8049",
+    Username = "Crissynuh8750",
+    Items = {
+        { Name = "ChangeCreatureColorsToken", ItemType = "Tokens" },
+        { Name = "CreatureReviveToken", ItemType = "Tokens" },
+        { Name = "PartialGrowToken", ItemType = "Tokens" },
+        { Name = "FullGrowToken", ItemType = "Tokens" },
+        { Name = "Shooms", ItemType = "Currency" }
+    }
+}
